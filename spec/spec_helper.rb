0a9585f692c3751d91a3569e4cf20e9417ce7d3b
Matt Brooke-Smith <matt@futureworkshops.com>
2013-12-10 13:37:11 +0000

    add spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path("../test_app/config/environment.rb",  __FILE__)
+require File.expand_path("../../lib/fwt_push_notification_server.rb",  __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'factory_girl_rails'
+require 'devise'
+
+Rails.backtrace_cleaner.remove_silencers!
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+FwtPushNotificationServer.delivery_method = :test
+
+RSpec.configure do |config|  
+  config.mock_with :rspec
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
+end
