e120fc97e95e415cdc76a5e51e6fb0225b954bf9
Matt Brooke-Smith <matt@futureworkshops.com>
2013-12-10 19:54:50 +0000

    Add test for multiple recipients

--- spec/notifyable_spec.rb
+++ spec/notifyable_spec.rb
@@ -1,16 +1,21 @@
 require 'spec_helper'
 
-class User < ActiveRecord::Base
-  devise :notifiable
-end
-
 describe User do
-  let(:user) { FactoryGirl.build(:user_with_apns_token) }
+  let(:user1) { FactoryGirl.build(:user_with_apns_token) }
+  let(:user2) { FactoryGirl.build(:user_with_apns_token) }
   
   it "sends a single push notification" do
-        
-    user.notify_once "Test"
+    user1.notify_once "Test"
     
-    FwtPushNotificationServer.deliveries[:apns].count.should == 1
+    FwtPushNotificationServer.deliveries.count.should == 1
+  end
+  
+  it "sends two push notifications at once" do
+    FwtPushNotificationServer.begin_transaction("Test push message") do
+      user1.schedule_notification
+      user2.schedule_notification
+    end
+    FwtPushNotificationServer.deliveries.count.should == 1
+    FwtPushNotificationServer.deliveries[0].recipients.count.should == 2
   end
 end
