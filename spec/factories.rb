11424bdd9e20c14b8646d8e512a55fcac0cd363c
Matt Brooke-Smith <matt@futureworkshops.com>
2013-12-19 15:57:51 +0300

    add user_with_invalid_apns_token to factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -15,4 +15,15 @@ FactoryGirl.define do
       apns_token.save!
     end
   end
+  
+  factory :user_with_invalid_apns_token, :class => User do
+    email
+    after(:build) do |user, evaluator|
+      apns_token = FactoryGirl.build(:apns_token)
+      apns_token.user_id = user.email
+      apns_token.is_valid = false
+      apns_token.save!
+    end
+  end
+  
 end
