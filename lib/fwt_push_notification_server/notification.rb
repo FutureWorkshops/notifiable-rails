5c145b4db538565c3e5d229b2745c733dbdeea75
Matt Brooke-Smith <matt@futureworkshops.com>
2013-12-20 16:51:18 +0300

    Add apns_message method to notification.rb

--- lib/fwt_push_notification_server/notification.rb
+++ lib/fwt_push_notification_server/notification.rb
@@ -6,5 +6,9 @@ module FwtPushNotificationServer
     def initialize(options = {})
       options.each {|k,v| instance_variable_set("@#{k}",v)}
     end
+    
+    def apns_message
+      @apns_message ||= (@message.bytesize > 232 ? "#{@message.byteslice(0, 229)}..." : @message)
+    end
   end
 end
