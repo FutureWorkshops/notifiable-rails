f387acf5ff2c94ceef98ea390f81c1a6da44f557
Matt Brooke-Smith <matt@futureworkshops.com>
2013-12-20 14:41:09 +0300

    Comment initialiser

--- lib/generators/fwt_push_notification_server/install/templates/initializer.rb
+++ lib/generators/fwt_push_notification_server/install/templates/initializer.rb
@@ -1,18 +1,49 @@
 
 FwtPushNotificationServer.configure do |config|
 
-	# APNS
-	config.apns_certificate = File.join(Rails.root, 'config', 'APNSDevelopment.pem')
-	config.apns_passphrase = 'PASSPHRASE'
-	config.apns_gateway = 'gateway.sandbox.push.apple.com'
-	
-	# GCM
-	config.gcm_api_key = 'YOUR-KEY-HERE'
-
-	# Devise integration
+  #
+	# Rails Engine
+  #
+  
+  # The controller class that the DeviceTokenController should extend
 	config.api_controller_class = ApplicationController
+  
+  # The class representing the holder of the device
 	config.user_class = User
-	config.user_key = :user_id
-	config.permitted_user_attributes = :first_name, :last_name
+  
+  # The key of the user, stored on the DeviceToken
+  # Defaults to email
+	#config.user_key = :email
+  
+  # Attributes permitted in the DeviceToken creation route
+  # These provide a way to enrich your server side data about a User
+  # Defaults to nil
+	#config.permitted_user_attributes = :first_name, :last_name
+
+  #
+	# APNS
+  #
+  
+  # The path to your apns private key
+	config.apns_certificate = File.join(Rails.root, 'config', 'apns-production.pem')
+  
+  # The passphrase for your private key
+  # Defaults to nil
+	#config.apns_passphrase = 'YOUR-PASSPHRASE-HERE'
+	
+  # The apns gateway to use
+  # Defaults to 'gateway.push.apple.com', can also be 'gateway.sandbox.push.apple.com'
+  #config.apns_gateway = 'gateway.push.apple.com'
+	
+  #
+	# GCM
+  #
+  
+  # Your GCM API Key
+	#config.gcm_api_key = 'YOUR-KEY-HERE'
+  
+  # The batch size
+  # Defaults to 1000
+  #config.gcm_batch_size = 1000
 
 end
