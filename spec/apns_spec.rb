4c2bd884b79e159ca0554d97e95e81757cf57c6d
Matt Brooke-Smith <matt@futureworkshops.com>
2013-12-20 11:16:16 +0300

    Add apns_spec

---
+++ spec/apns_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe FwtPushNotificationServer::Notifier::APNS do
+  let(:user1) { FactoryGirl.build(:user_with_apns_token) }
+  let(:notification) { FwtPushNotificationServer::Notification.new(:message => "Test message")}
+  
+  it "sends a single grocer notification" do    
+    
+    user1.send_notification(notification)
+    
+    Timeout.timeout(3) {
+      notification = @grocer.notifications.pop
+      expect(notification.alert).to eq("Test message")
+    }
+  end
+  
+end
