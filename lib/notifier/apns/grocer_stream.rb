8320612644d25d3005497f2b683f9bd540aa588a
Matt Brooke-Smith <matt@futureworkshops.com>
2013-12-23 08:23:07 +0000

    Move notifiers into modules

---
+++ lib/notifier/apns/grocer_stream.rb
@@ -0,0 +1,47 @@
+module Notifiable
+	module Notifier
+    module APNS
+  		class GrocerStream < Base
+      
+        def close
+          super
+          @grocer_pusher = nil        
+          @grocer_feedback = nil
+        end
+      
+  			protected      
+  			def enqueue(notification, device_token)        				
+          
+          grocer_notification = Grocer::Notification.new(device_token: device_token.token, alert: notification.apns_message, custom: notification.payload)
+  				grocer_pusher.push(grocer_notification) unless Notifiable.delivery_method == :test
+
+          processed(notification, device_token)
+  			end
+      
+        def flush
+          process_feedback unless Notifiable.env == 'test'
+        end
+
+        private 
+        def grocer_pusher
+          @grocer_pusher ||= Grocer.pusher(Notifiable.apns_gateway_config)
+        end
+      
+        def grocer_feedback
+  				@grocer_feedback ||= Grocer.feedback(Notifiable.apns_feedback_config)
+        end
+      
+        def process_feedback
+  				grocer_feedback.each do |attempt|
+  					token = attempt.device_token
+  					device_token = DeviceToken.find_by_token(token)
+  					if device_token
+  						device_token.update_attribute("is_valid", false) if device_token.updated_at < attempt.timestamp
+  						Rails.logger.info("Device #{token} (#{device_token.user_id}) failed at #{attempt.timestamp}")
+  					end
+  				end
+        end
+  		end
+    end
+	end
+end
---
+++ lib/notifier/gcm/gcm_batch.rb
@@ -0,0 +1,48 @@
+module Notifiable
+	module Notifier
+    module GCM
+  		class GCMBatch < Base
+      
+        def initialize
+          @batch = {}
+        end
+      
+        # todo should be made threadsafe
+        protected 
+  			def enqueue(notification, device_token)
+          @batch[notification] ||= [device_token]        								
+          tokens = @batch[notification]
+          if tokens.count >= Notifiable.gcm_batch_size
+            send_batch(notification, tokens)
+          end
+    		end
+      
+        def flush
+          @batch.each_pair do |notification, device_tokens|
+            send_batch(notification, device_tokens)
+          end
+        end
+
+  			private
+  			def send_batch(notification, device_tokens)
+          if Notifiable.delivery_method == :test || Notifiable.env == 'test'
+            device_tokens.each {|d| processed(notification, d)}
+          else
+    				gcm = ::GCM.new(Notifiable.gcm_api_key)
+    				response = gcm.send_notification(device_tokens.collect{|dt| dt.token}, {:data => {:message => notification.message}})
+    				body = JSON.parse(response.fetch(:body, "{}"))
+    				results = body.fetch("results", [])
+    				results.each_with_index do |result, idx|
+    					if result["error"]
+    						device_tokens[idx].update_attribute('is_valid', false)
+              else
+                processed(notification, device_tokens[idx])
+              end
+    				end          
+          end
+          @batch[notification] = nil
+  			end
+  		end
+    end
+	end
+end
