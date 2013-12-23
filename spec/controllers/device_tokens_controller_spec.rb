c385090469ac411643c50b930a5941a7c0f2c204
Matt Brooke-Smith <matt@futureworkshops.com>
2013-12-23 00:26:55 +0000

    Add pending spec for updating user attributes

--- spec/controllers/device_tokens_controller_spec.rb
+++ spec/controllers/device_tokens_controller_spec.rb
@@ -5,10 +5,13 @@ describe FwtPushNotificationServer::DeviceTokensController do
   let(:user1) { FactoryGirl.create(:user) }
   
   it "creates a new device token" do    
-    
     post :create, :token => "ABC123", :user_id => user1.email, :provider => :apns
     FwtPushNotificationServer::DeviceToken.count.should == 1
-    
+    FwtPushNotificationServer::DeviceToken.first.user_id.should eql user1.email
+  end
+  
+  it "updates user attributes" do    
+    pending("update the test app to include first name/last name for user")
   end
   
 end
