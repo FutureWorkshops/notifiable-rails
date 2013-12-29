62fec7aa655aecf941a8c202eefc80441d60fef0
Matt Brooke-Smith <matt@futureworkshops.com>
2013-12-29 23:48:33 +0000

    Notification has many notification_device_tokens

--- lib/notifiable/notification.rb
+++ lib/notifiable/notification.rb
@@ -1,7 +1,7 @@
 module Notifiable
   class Notification < ActiveRecord::Base
     
-    has_many :notification_recipients
+    has_many :notification_device_tokens, :class_name => 'Notifiable::NotificationDeviceToken'
     
     def apns_message
       @apns_message ||= (self.message.bytesize > 232 ? "#{self.message.byteslice(0, 229)}..." : self.message)
