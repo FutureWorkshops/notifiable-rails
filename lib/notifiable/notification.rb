0a78cd8e322715fafc1d2d726bef53c5d656813e
Matt Brooke-Smith <matt@futureworkshops.com>
2013-12-29 00:07:01 +0000

    Notification is an active record

--- lib/notifiable/notification.rb
+++ lib/notifiable/notification.rb
@@ -1,14 +1,7 @@
 module Notifiable
-  class Notification
-    attr_accessor :message
-    attr_accessor :payload
-    
-    def initialize(options = {})
-      options.each {|k,v| instance_variable_set("@#{k}",v)}
-    end
-    
+  class Notification < ActiveRecord::Base
     def apns_message
-      @apns_message ||= (@message.bytesize > 232 ? "#{@message.byteslice(0, 229)}..." : @message)
+      @apns_message ||= (self.message.bytesize > 232 ? "#{self.message.byteslice(0, 229)}..." : self.message)
     end
   end
 end
