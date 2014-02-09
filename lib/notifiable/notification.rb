3e6b98ea8cf6f1590b5e9fcedf43095bfbe31c72
Matt Brooke-Smith <matt@futureworkshops.com>
2014-02-09 16:39:15 +0400

    Remove apns_message method from notification

--- lib/notifiable/notification.rb
+++ lib/notifiable/notification.rb
@@ -5,8 +5,5 @@ module Notifiable
     
     has_many :notification_device_tokens, :class_name => 'Notifiable::NotificationDeviceToken'
     
-    def apns_message
-      @apns_message ||= (self.message.bytesize > 232 ? "#{self.message.byteslice(0, 229)}..." : self.message)
-    end
   end
 end
