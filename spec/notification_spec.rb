eebaf172c14bfe3be59f132de36c16a8fad3e33b
Matt Brooke-Smith <matt@futureworkshops.com>
2014-02-09 16:40:42 +0400

    Spec for property overrides

---
+++ spec/notification_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Notifiable::Notification do
+  
+  it "stores a message" do    
+    Notifiable::Notification.create :message => "Test message"
+    
+    Notifiable::Notification.first.provider_value(:mock, :message).should eql "Test message"
+  end
+  
+  it "overrides a message" do    
+    Notifiable::Notification.create :message => "Test message", :payload => {:mock => {:message => "A different message"}}
+    
+    Notifiable::Notification.first.provider_value(:mock, :message).should eql "A different message"
+  end
+  
+  it "returns nil for a property that doesnt exist" do    
+    Notifiable::Notification.create :message => "Test message"
+    
+    Notifiable::Notification.first.provider_value(:mock, :sound).should be_nil
+  end
+  
+end
