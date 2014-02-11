eaf7443bfcf161b7b3af273057b4b1efaa891dcc
Matt Brooke-Smith <matt@futureworkshops.com>
2014-02-11 19:38:15 +0300

    Update specs

--- spec/notification_spec.rb
+++ spec/notification_spec.rb
@@ -3,27 +3,15 @@ require 'spec_helper'
 describe Notifiable::Notification do
   
   it "stores a message" do    
-    Notifiable::Notification.create :message => "Test message"
+    Notifiable::Notification.create(:message => "Test message")
     
-    Notifiable::Notification.first.provider_value(:mock, :message).should eql "Test message"
+    Notifiable::Notification.first.message.should eql "Test message"
   end
   
-  it "overrides a message" do    
-    Notifiable::Notification.create :message => "Test message", :payload => {:mock => {:message => "A different message"}}
+  it "stores params" do    
+    Notifiable::Notification.create :params => {:custom_property => "A different message"}
     
-    Notifiable::Notification.first.provider_value(:mock, :message).should eql "A different message"
-  end
-  
-  it "returns a hash" do    
-    Notifiable::Notification.create :payload => {:mock => {:custom => {:custom_property => "A different message"}}}
-    
-    Notifiable::Notification.first.provider_value(:mock, :custom).should == {:custom_property => "A different message"}
-  end
-  
-  it "returns nil for a property that doesnt exist" do    
-    Notifiable::Notification.create :message => "Test message"
-    
-    Notifiable::Notification.first.provider_value(:mock, :sound).should be_nil
+    Notifiable::Notification.first.params.should == {:custom_property => "A different message"}
   end
   
 end
