9bc901a13e2f2d20a2943fa1c204265d6b129af6
Matt Brooke-Smith <matt@futureworkshops.com>
2014-02-11 19:38:40 +0300

    notification only serialises params

--- lib/notifiable/notification.rb
+++ lib/notifiable/notification.rb
@@ -1,16 +1,8 @@
 module Notifiable
   class Notification < ActiveRecord::Base
     
-    serialize :payload
+    serialize :params
     
     has_many :notification_device_tokens, :class_name => 'Notifiable::NotificationDeviceToken'
-    
-    def provider_value(provider, key)
-      if self.payload && self.payload[provider] && self.payload[provider][key]
-        self.payload[provider][key]
-      elsif self.respond_to? key
-        self.send(key)
-      end       
-    end
   end
 end
