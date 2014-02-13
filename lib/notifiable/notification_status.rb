18731273ebc89510d945daec6504db00892aabc9
Matt Brooke-Smith <matt@futureworkshops.com>
2014-02-13 17:39:00 +0300

    Rename class

--- lib/notifiable/notification_status.rb
+++ lib/notifiable/notification_status.rb
@@ -1,5 +1,5 @@
 module Notifiable
-  class NotificationDeviceToken < ActiveRecord::Base
+  class NotificationStatus < ActiveRecord::Base
     belongs_to :notification, :class_name => 'Notifiable::Notification'
     belongs_to :device_token, :class_name => 'Notifiable::DeviceToken'
   end
