a6c3d9b8aee828b943fde347619e36e2468c6269
Matt Brooke-Smith <matt@futureworkshops.com>
2014-03-05 19:59:12 +0000

    Add Notifiable::App

---
+++ lib/notifiable/app.rb
@@ -0,0 +1,8 @@
+module Notifiable
+  class App < ActiveRecord::Base
+    has_many :device_tokens, :class_name => 'Notifiable::DeviceToken', :dependent => :destroy
+    has_many :notifications, :class_name => 'Notifiable::App', :dependent => :destroy
+    
+    serialize :configuration
+  end
+end
