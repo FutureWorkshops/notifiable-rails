8862e807cfe7db3252084d8dae9d2824bfa65f83
Matt Brooke-Smith <matt@futureworkshops.com>
2014-03-12 18:56:59 +0300

    Add opened! and opened? methods to notification_status

--- lib/notifiable/notification_status.rb
+++ lib/notifiable/notification_status.rb
@@ -2,5 +2,13 @@ module Notifiable
   class NotificationStatus < ActiveRecord::Base
     belongs_to :notification, :class_name => 'Notifiable::Notification'
     belongs_to :device_token, :class_name => 'Notifiable::DeviceToken'
+    
+    def opened!
+      update_attributes({:status => -1, :uuid => nil})
+    end
+    
+    def opened?
+      self.status == -1
+    end
   end
 end
