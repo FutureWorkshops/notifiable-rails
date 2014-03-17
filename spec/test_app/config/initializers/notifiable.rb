e16157c0350dd099b84e0f9ee2ac68839b4d2ea6
Matt Brooke-Smith <matt@futureworkshops.com>
2014-03-17 00:10:42 +0000

    Remove count opens option on notifiable

--- spec/test_app/config/initializers/notifiable.rb
+++ spec/test_app/config/initializers/notifiable.rb
@@ -9,6 +9,4 @@ Notifiable.configure do |config|
   # Set the delivery method to test, preventing notifications from being sent
   # Defaults to :send
   #config.delivery_method = :test
-
-  config.count_opens = true
 end
