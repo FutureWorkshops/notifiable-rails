11eb68461d3d1810d38d245111f7d66d801cd920
Matt Brooke-Smith <matt@futureworkshops.com>
2014-04-14 23:46:31 +0100

    Add setting to initializer.rb template

--- lib/generators/notifiable/install/templates/initializer.rb
+++ lib/generators/notifiable/install/templates/initializer.rb
@@ -10,6 +10,11 @@ Notifiable.configure do |config|
   # The class representing the holder of the device
 	config.user_class = User
   
+  # The size of the batch of Notification Statuses kept in memory
+  # before being saved. This should be varied with the heap size of the process
+  # sending the notifications. Defaults to 10,000.
+  #config.notification_status_batch_size = 10000
+  
   # Set the delivery method to test, preventing notifications from being sent
   # Defaults to :send
   #config.delivery_method = :test
