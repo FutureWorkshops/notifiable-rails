7f16af5431b54bf40e43336a2213b34f90133c6e
Matt Brooke-Smith <matt@futureworkshops.com>
2015-02-22 19:39:08 +0000

    Rename notifications_controller --> notification_statuses_controller

--- app/controllers/notifiable/notification_statuses_controller.rb
+++ app/controllers/notifiable/notification_statuses_controller.rb
@@ -1,29 +1,25 @@
-
 module Notifiable
-  
-  class NotificationsController < Notifiable.api_controller_class
-    
+  class NotificationStatusesController < Notifiable.api_controller_class
     before_filter :find_notification_status!, :check_authorisation!
-    
+  
     def opened      
-      if @notification_status.opened! && @notification_status.notification.increment!(:opened_count)
+      if @notification_status.opened!
         head :status => :ok
       else
         render :json => { :errors => @notification_status.errors.full_messages }, :status => :unprocessable_entity
       end
-      
-    end
     
+    end
+  
     private
     def find_notification_status!
-      return head :status => :not_acceptable unless params[:notification_id] && params[:device_token_id]
-            
-      @notification_status = NotificationStatus.find_by!("notification_id = ? AND device_token_id = ?", params[:notification_id], params[:device_token_id])
+      return head :status => :not_acceptable unless params[:localized_notification_id] && params[:device_token_id]
+          
+      @notification_status = NotificationStatus.find_by_localized_notification_id_and_device_token_id!(params[:localized_notification_id], params[:device_token_id])
     end
-    
+  
     def check_authorisation!
       head :status => :unauthorized unless current_notifiable_user == @notification_status.device_token.user
     end
   end
-  
 end
