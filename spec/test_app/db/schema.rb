002ca816507b47a6d965fd9eb4a043b181e7a7a0
Matt Brooke-Smith <matt@futureworkshops.com>
2015-02-22 19:37:17 +0000

    Update test app schema

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131210115652) do
+ActiveRecord::Schema.define(version: 20131210115660) do
 
   create_table "notifiable_apps", force: true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20131210115652) do
   create_table "notifiable_device_tokens", force: true do |t|
     t.string   "token"
     t.string   "provider"
+    t.string   "locale"
     t.boolean  "is_valid",   default: true
     t.integer  "user_id"
     t.integer  "app_id"
@@ -33,9 +34,16 @@ ActiveRecord::Schema.define(version: 20131210115652) do
   add_index "notifiable_device_tokens", ["token"], name: "index_notifiable_device_tokens_on_token", unique: true
   add_index "notifiable_device_tokens", ["user_id"], name: "index_notifiable_device_tokens_on_user_id"
 
-  create_table "notifiable_notifications", force: true do |t|
+  create_table "notifiable_localized_notifications", force: true do |t|
     t.text     "message"
     t.text     "params"
+    t.string   "locale"
+    t.integer  "notification_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "notifiable_notifications", force: true do |t|
     t.integer  "app_id"
     t.integer  "sent_count",             default: 0
     t.integer  "gateway_accepted_count", default: 0
@@ -45,7 +53,7 @@ ActiveRecord::Schema.define(version: 20131210115652) do
   end
 
   create_table "notifiable_statuses", force: true do |t|
-    t.integer  "notification_id"
+    t.integer  "localized_notification_id"
     t.integer  "device_token_id"
     t.integer  "status"
     t.datetime "created_at"
