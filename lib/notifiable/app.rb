d84d9b0621e325129e79f6eec654e359c3cff815
Matt Brooke-Smith <matt@futureworkshops.com>
2016-03-12 09:25:58 +0000

    Update app
    Add save_notification_statuses

--- lib/notifiable/app.rb
+++ lib/notifiable/app.rb
@@ -10,5 +10,14 @@ module Notifiable
       
       self.configuration[provider].each_pair {|key, value| notifier.send("#{key}=", value) if notifier.methods.include?("#{key}=".to_sym) } 
     end
+    
+    def save_notification_statuses?
+      self.configuration[:save_notification_statuses].nil? ? true : self.configuration[:save_notification_statuses]
+    end
+    
+    def save_notification_statuses=(save_notification_statuses)
+      self.configuration[:save_notification_statuses] = save_notification_statuses
+    end
+    
   end
 end
