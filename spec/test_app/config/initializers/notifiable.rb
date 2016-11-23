63008cda69c93d986d321ca9a8f593962db4ff49
Matt Brooke-Smith <matt@futureworkshops.com>
2016-11-24 01:05:29 +0400

    config

--- spec/test_app/config/initializers/notifiable.rb
+++ spec/test_app/config/initializers/notifiable.rb
@@ -3,9 +3,6 @@ Notifiable.configure do |config|
   # The controller class that the DeviceTokenController should extend
 	config.api_controller_class = ApplicationController
   
-  # The class representing the holder of the device
-	config.user_class = User
-  
   # Set the delivery method to test, preventing notifications from being sent
   # Defaults to :send
   #config.delivery_method = :test
