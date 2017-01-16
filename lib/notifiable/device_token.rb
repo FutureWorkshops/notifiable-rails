77f6bc401ab0601a29c02ba94906a533c0cc3bd3
Matt Brooke-Smith <matt@futureworkshops.com>
2017-01-16 22:51:59 +0000

    model

--- lib/notifiable/device_token.rb
+++ lib/notifiable/device_token.rb
@@ -1,3 +1,10 @@
+require 'rgeo'
+require 'rgeo-activerecord'
+
+RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
+  config.default = RGeo::Geographic.spherical_factory(srid: 4326)
+end
+
 module Notifiable
   class DeviceToken < ActiveRecord::Base
     belongs_to :app, :class_name => "Notifiable::App"
@@ -7,6 +14,6 @@ module Notifiable
     validates :provider, presence: true
     validates :app, presence: true
     
+    scope :nearby, -> (lon, lat, radius){ where("ST_DWithin(lonlat, ST_MakePoint(?,?), ?)", lon, lat, radius) }    
   end
-
 end
