c351d0ee1471da3d14e24eded2ae0c0071cce464
Matt Brooke-Smith <matt@futureworkshops.com>
2018-06-24 17:20:21 +0100

    Add category to notifications

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 201806182135000) do
+ActiveRecord::Schema.define(version: 201806242135000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 201806182135000) do
     t.string "name"
     t.geography "lonlat", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}
     t.string "language"
-    t.string "country"
+    t.string "region"
     t.index ["app_id", "token"], name: "index_notifiable_device_tokens_on_app_id_and_token", unique: true
     t.index ["user_alias"], name: "index_notifiable_device_tokens_on_user_alias"
   end
@@ -56,7 +56,8 @@ ActiveRecord::Schema.define(version: 201806182135000) do
     t.integer "badge_count"
     t.text "last_error_message"
     t.string "title"
-    t.string "thead_id"
+    t.string "thread_id"
+    t.string "category"
   end
 
   create_table "notifiable_statuses", id: :serial, force: :cascade do |t|
